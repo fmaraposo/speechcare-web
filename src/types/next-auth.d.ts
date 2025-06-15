10ccce387f175bd90c6ff47cb7c6b74330b2897a
Francisco Martins <f.martins@crowdfarming.com>
2025-06-15 19:52:11 +0200

    feat: add custom types for next-auth user and session, and update tsconfig for improved module resolution

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,23 @@
+import { DefaultSession, DefaultUser } from "next-auth";
+import { UserRoles } from "./user/user-roles.enum";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      role: string;
+    } & DefaultSession["user"];
+  }
+
+  interface User extends DefaultUser {
+    role: UserRoles;
+    id: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    role: UserRoles;
+  }
+}
