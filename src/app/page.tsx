335e4fabde834ba842cf23150a7b0b250eda0be3
patricia <patricia.ramos@galeo.tech>
2025-07-16 10:47:31 +0100

    folder structure in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home(): JSX.Element {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/images/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -33,7 +33,7 @@ export default function Home(): JSX.Element {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="/images/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -57,7 +57,7 @@ export default function Home(): JSX.Element {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
+          <Image aria-hidden src="/images/file.svg" alt="File icon" width={16} height={16} />
           Learn
         </a>
         <a
@@ -66,7 +66,7 @@ export default function Home(): JSX.Element {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
+          <Image aria-hidden src="/images/window.svg" alt="Window icon" width={16} height={16} />
           Examples
         </a>
         <a
@@ -75,7 +75,7 @@ export default function Home(): JSX.Element {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
+          <Image aria-hidden src="/images/globe.svg" alt="Globe icon" width={16} height={16} />
           Go to nextjs.org →
         </a>
       </footer>
