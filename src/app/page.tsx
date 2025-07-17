1f13063733b292725e15a44f5eb010ea90fac53f
patricia <patricia.ramos@galeo.tech>
2025-07-17 12:57:15 +0100

    common rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect } from 'react';
 
 export default function Home(): JSX.Element {
   useEffect(() => {
-    console.log("Home component mounteddd");
+    console.log("Home component mounteddddd");
   }, []);
 
   return (
