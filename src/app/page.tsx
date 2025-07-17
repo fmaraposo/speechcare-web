fdb2d1dfb14e5e8785d38f83e4ebaf5ee3de846b
patricia <patricia.ramos@galeo.tech>
2025-07-17 11:12:02 +0100

    Hs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { JSX } from "react";
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 
-
+const Unused_Var = 123;
 export default function Home(): JSX.Element {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
