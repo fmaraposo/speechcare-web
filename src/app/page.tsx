3e193e4b3fd10477cecb1d8b75d89e346a92dc86
patricia <patricia.ramos@galeo.tech>
2025-07-17 14:42:00 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
 import Image from "next/image";
 import { JSX } from "react";
-import React, { useEffect } from 'react';
 
 export default function Home(): JSX.Element {
-  useEffect(() => {
-    console.warn("Home component mounteddddd");
-    console.log('dhdddsddddddddddddddddddddddh')
-  }, []);
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[32px]row - start - 2 items - center sm: items - start">
         <Image
           className="dark:invert"
           src="/images/next.svg"
